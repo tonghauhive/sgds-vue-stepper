551c5841867b8ba50c747ddc6db6de9ce3939b99
Tong Hau <tonghau_Lee@hive.gov.sg>
2024-10-23 11:25:53 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import "@govtechsg/sgds-web-component/themes/day.css"
+import "@webcomponents/scoped-custom-element-registry"
+import "@govtechsg/sgds-web-component"
+import "./components/file-upload/index.umd.js"
+import Footer from './components/Footer.vue'
+import Mainnav from './components/Mainnav.vue'
+import Masthead from './components/Masthead.vue'
+import Stepper from './components/Stepper.vue'
+import Breadcrumb from './components/Breadcrumb.vue'
+</script>
+
+<template>
+  <Masthead />
+  <Mainnav />
+
+  <div class="container">
+    <sgds-template-grid>
+      <sgds-content-area>
+        <sgds-content-body>
+          <Breadcrumb />
+          <h2>Name of e-Service</h2>
+          <Stepper />
+        </sgds-content-body>
+      </sgds-content-area>
+    </sgds-template-grid>
+  </div>
+  <Footer />
+</template>
+
+<style scoped>
+div.container {
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  padding: 2rem;
+  max-width: 70rem;
+}
+
+@media (min-width: 48rem) {
+  div.container {
+    max-width: 73rem;
+  }
+}
+</style>
---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,7 @@
+<template>
+    <sgds-breadcrumb arialabel="breadcrumb">
+        <sgds-breadcrumb-item rel="noreferrer noopener" href="#">Home</sgds-breadcrumb-item>
+        <sgds-breadcrumb-item href="#">Item 1</sgds-breadcrumb-item>
+        <sgds-breadcrumb-item>Last Item</sgds-breadcrumb-item>
+    </sgds-breadcrumb>
+</template>
